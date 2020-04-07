77fd9b1a935dc8eea173f2c85a5a244720743106
Atif Aziz <code@raboof.com>
2020-04-07 21:49:03 +0200

    Remove requirement to cd in bash scripts

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-[[ -e test.sh ]] || { echo >&2 "Please cd into the script location before running it."; exit 1; }
 set -e
+cd "$(dirname "$0")"
 ./build.sh
 for c in Debug Release; do
     dotnet test --no-build tests -c $c
